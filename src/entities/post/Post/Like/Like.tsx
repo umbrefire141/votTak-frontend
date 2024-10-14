3d6dfca90f974090fcccc09e4a55789489516c14
Matvey <matvejaksentev80@gmail.com>
2024-10-14 20:33:36 +0700

    Func likes

--- src/entities/post/Post/Like/Like.tsx
+++ src/entities/post/Post/Like/Like.tsx
@@ -1,13 +1,47 @@
+import postsService from '@/shared/api/posts.service';
+import { useUserStore } from '@/shared/model/user.store';
+import clsx from 'clsx';
+import { useEffect, useState } from 'react';
 import { AiOutlineLike } from 'react-icons/ai';
 import { ILikeComponent } from './Like.interface';
 
-const Like = ({ likes }: ILikeComponent) => {
+const Like = ({ uuid, likes }: ILikeComponent) => {
+	const { user: currentUser } = useUserStore();
+	const [active, setActive] = useState(false);
+	const [countLikes, setCountLikes] = useState(likes.length);
+
+	const userId = likes.find(({ user }) => user.uuid === currentUser?.uuid)?.user
+		.uuid;
+
+	const handleActionsPost = async () => {
+		if (active) {
+			setCountLikes(countLikes - 1);
+			setActive(false);
+			await postsService.unlikePost(uuid);
+		} else {
+			setCountLikes(countLikes + 1);
+			setActive(true);
+			await postsService.likePost(uuid);
+		}
+	};
+
+	useEffect(() => {
+		if (userId) setActive(true);
+	}, [userId]);
+
 	return (
-		<div className="cursor-pointer flex items-center gap-1 rounded-md p-1 ">
-			<AiOutlineLike className="w-6 h-6" />
-			<p className="text-lg">{likes}</p>
+		<button
+			className="cursor-pointer flex items-center gap-1 rounded-md p-1 "
+			onClick={handleActionsPost}
+		>
+			<AiOutlineLike
+				className={clsx('w-6 h-6', {
+					['text-red-500']: active,
+				})}
+			/>
+			<p className="text-lg">{countLikes}</p>
 			<p className="text-base">Likes</p>
-		</div>
+		</button>
 	);
 };
 
